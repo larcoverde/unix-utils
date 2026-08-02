48754607b247b36d12e9650bf57418c4722b539e
Lucas Arcoverde <larcoverde@proton.me>
2026-08-02 10:49:04 -0300

    feat(echo): make help message

--- src/echo.c
+++ src/echo.c
@@ -9,6 +9,14 @@ usage()
     printf("usage: echo <opt> <input>\n");
 }
 
+void
+help()
+{
+    printf("echo - writes input text to standard output\n\n");
+    usage();
+    print("             -n    do not output the trailing newline\n\n");
+}
+
 int
 main(int argc, char **argv)
 {
